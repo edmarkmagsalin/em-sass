fe731e9b34d8aac728f53c66916c070a268e8a3c
Edmark Magsalin <edmarkmagsalin@gmail.com>
2020-06-12 13:23:56 +0800

    Input updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-const body = document.body;
+// dark mode togggle
 const toggle = document.querySelector('#darkmode');
-
 setTheme = () => {
     let dataTheme = localStorage.getItem("data-theme") || "default";
     dataTheme === "dark" && toggle.setAttribute("checked", "true")
-    body.setAttribute("data-theme", dataTheme)
+    document.body.setAttribute("data-theme", dataTheme)
 }
 setTheme();
 
@@ -13,3 +12,21 @@ switchTheme = () => {
     setTheme();
 }
 toggle.addEventListener('input', switchTheme);
+
+// place label
+const labeledInputs = document.querySelectorAll('.labeled-input > input[type=text], .labeled-input > input[type=email], .labeled-input > input[type=password], .labeled-input > input[type=number]');
+
+function placeLabel () {
+    if(this.value) {
+        this.classList.remove('place-label');
+    }
+    else {
+        this.classList.add('place-label');
+    }
+}
+
+labeledInputs.forEach(labeledInput => {
+    labeledInput.classList.add('place-label');
+    labeledInput.addEventListener('focus', placeLabel);
+    labeledInput.addEventListener('input', placeLabel);
+});
